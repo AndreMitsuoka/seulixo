6fb8aa8c2e9aaccbeafcd62f56e7e0ad3d5d30a3
Mitsuoka <mitsuoka@ubuntu.ubuntu-domain>
2012-12-01 22:15:03 -0300

    arrumando porquicess mta porcas

--- app/models/idh.rb
+++ app/models/idh.rb
@@ -1,13 +1,11 @@
 #encoding: UTF-8
 require "csv"
-#require "localidades.rb"
 
 class Idh < ActiveRecord::Base
   attr_accessible :owner,:idhm,:renda,:longevidade,:educacao,:ano
 
 	def read_in_csv_idh
-		#CSV=IO.read(file).force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
-		csv_text = File.read('saida.csv')
+		csv_text = File.read('saidaIDH.csv')
 		csv = CSV.parse(csv_text, :headers => true)
 		csv.each do |row|
 			row = row.to_hash.with_indifferent_access
@@ -19,10 +17,7 @@ class Idh < ActiveRecord::Base
 			id = Localidade.get_local_id(lnome)
 			puts id
 
-			if id != 0
-				p "merda"
-			else
-				p "cadastrando \n"
+			if id == 0
 				@localidade.save
 				id = @localidade.id
 			end
